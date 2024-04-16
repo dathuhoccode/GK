b4f836c215838414989017d379d798458fb46716
dathuhoccode <syaoran2k5@gmail.com>
2024-04-16 16:48:08 +0700

    a

---
+++ src/Experience.java
@@ -0,0 +1,18 @@
+public class Experience extends employee {
+    private int expInYear;
+    private String proSkill;
+
+    public Experience(String id, String fullName, String birthDay, String phone, String email, String employeeType,
+                      int expInYear, String proSkill) {
+        super(id, fullName, birthDay, phone, email, employeeType);
+        this.expInYear = expInYear;
+        this.proSkill = proSkill;
+    }
+
+    @Override
+    public void showInfo() {
+        super.showInfo();
+        System.out.println("Experience In Year: " + expInYear);
+        System.out.println("Professional Skill: " + proSkill);
+    }
+}
